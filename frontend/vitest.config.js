c9bb51389ba589a40a0cd45799d8db1bc3b4d0e2
らぐな <pasupoto17@gmail.com>
2026-04-09 10:52:25 +0900

    Initial commit

---
+++ frontend/vitest.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: 'jsdom', // ← これで localStorage や DOM が使える
+    },
+});
