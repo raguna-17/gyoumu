7e64faf0a1699a2b1d8b563dac14ebd4b2fa2efc
らぐな <pasupoto17@gmail.com>
2026-04-10 18:57:06 +0900

    Initial commit

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserCreateView, UserListView, MeView
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
